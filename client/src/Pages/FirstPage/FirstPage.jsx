cd2e64c1a4692e49fea5f151e338c2bfa1e5b018
AJIT-KUMAR-PANDIT <ajitkumarpandit1999@gmail.com>
2024-04-22 19:24:48 +0530

    update first page blink

--- client/src/Pages/FirstPage/FirstPage.jsx
+++ client/src/Pages/FirstPage/FirstPage.jsx
@@ -1,14 +1,54 @@
-import React from 'react'
-import Logo from '../../Utils/Logo/Logo.jsx'
+import Logo from "../../Utils/Logo/Logo.jsx";
+import React, { useState, useEffect } from "react";
 
 const FirstPage = () => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsVisible(false);
+    }, 1000); // 3000 milliseconds = 3 seconds
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
-    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', width: '100vw', backgroundColor: '#fff1ca'}}>
-      <Logo  height={"200px"} width={"200px"}/> 
-    </div>
+      {isVisible ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+            width: "100vw",
+            backgroundColor: "#fff1ca",
+          }}
+        >
+          <Logo height={"200px"} width={"200px"} />
+        </div>
+      ) : (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+            width: "100vw",
+            backgroundImage: `url("https://i.pinimg.com/originals/2b/33/21/2b33214fa0b8e8197f11ce2c43136dba.jpg")`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
+          <div
+            style={{ borderRadius: "50%", backgroundColor: "#fff1ca" }}
+          >
+            <Logo height={"91px"} width={"91px"} />
+          </div>
+        </div>
+      )}
     </>
-  )
-}
+  );
+};
 
 export default FirstPage;
