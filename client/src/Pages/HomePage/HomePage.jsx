ba6ca819bff2cebef16c68c46a24d1bf95e67de0
AJIT-KUMAR-PANDIT <ajitkumarpandit1999@gmail.com>
2024-05-16 16:20:16 +0530

    update

--- client/src/Pages/HomePage/HomePage.jsx
+++ client/src/Pages/HomePage/HomePage.jsx
@@ -11,7 +11,7 @@ const HomePage = () => {
   useEffect(
     () => {
       axios
-        .get("https://smartgroceries.nakprc.com/src/assets/fruitsNveg.json")
+        .get("https://smartgroceries.nakprc.com/fruitsNveg.json")
         .then((response) => {
           setGroceryItem(response.data);
         })
