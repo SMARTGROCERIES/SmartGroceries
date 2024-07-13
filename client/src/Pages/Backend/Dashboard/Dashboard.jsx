49a9759d119f7c46c51bab4961c51846d7f837bd
AJIT-KUMAR-PANDIT <ajitkumarpandit1999@gmail.com>
2024-07-13 18:33:59 +0530

    "Added @heroicons/react dependency, updated Dashboard component with new UI elements"

--- client/src/Pages/Backend/Dashboard/Dashboard.jsx
+++ client/src/Pages/Backend/Dashboard/Dashboard.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import SideBar from "../../../Components/Backend/SideBar/SideBar";
+import LogoSmartGroceries from "../../../Utils/Logo/Logo";
 
 const Dashboard = () => {
   return (
     <>
-      <div>Dashboard</div>
+      <div className="bg-[azure] flex items-center gap-31 ">
+        <div>
+          <SideBar />
+        </div>
+        <div className="flex gap-31">
+          <div>
+            <LogoSmartGroceries width={"51px"} height={"51px"} />
+          </div>
+          <div className="text-[4vh] ">Admin Pannel</div>
+        </div>
+      </div>
+      <div className="flex">
+        <div></div>
+      </div>
     </>
   );
 };
